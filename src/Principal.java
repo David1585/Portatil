a220e78d3908c2d616b33f4a245950acc3fbcfbc
David1585 <100976020+David1585@users.noreply.github.com>
2022-08-18 17:25:33 -0500

    se genera la rama Leidy y se inserta un pequeño programa

--- src/Principal.java
+++ src/Principal.java
@@ -1,8 +1,15 @@
+import javax.swing.*;
+
 public class Principal {
 
     public static void main(String[] args) {
 
         System.out.println("hola mundo");
+
+        int numero = Integer.parseInt(JOptionPane.showInputDialog("digite un numero"));
+
+        JOptionPane.showMessageDialog(null, "el numero ingresado fue : "+ numero);
+
 
     }
 }
